e6f2b81d5acd5bec72b7562f61542d9d95143b3e
lulilusu <13594536446@163.com>
2019-12-19 17:54:07 +0800

    Initial commit

---
+++ 2-extentreports/src/main/java/com/test/ExtenedDemo.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ */
+public class ExtenedDemo {
+
+    @Test
+   public void test1(){
+       Assert.assertEquals(1,1);
+   }
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals(1,1);
+    }
+
+    public void reportLog(){
+        Reporter.log("自己的日志");
+        throw new RuntimeException("自己的异常");
+    }
+}
