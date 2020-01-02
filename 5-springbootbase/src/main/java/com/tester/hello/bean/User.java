d13f4cdac9167c8f5c871b7e0a19a9215635a0e5
git <jh4466@outlook.com>
2020-01-02 21:38:24 +0800

    add User.java PostCookiesMethod.java

---
+++ 5-springbootbase/src/main/java/com/tester/hello/bean/User.java
@@ -0,0 +1,12 @@
+package com.tester.hello.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String passWord;
+    private String name;
+    private String sex;
+    private String age;
+}
