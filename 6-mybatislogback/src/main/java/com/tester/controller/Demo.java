4ba28366a8e6857b5c8ad73e9f1fea95d46f8dd3
lulilusu <13594536446@163.com>
2020-01-04 17:58:37 +0800

    updata 6 add 7

--- 6-mybatislogback/src/main/java/com/tester/controller/Demo.java
+++ 6-mybatislogback/src/main/java/com/tester/controller/Demo.java
@@ -1,12 +1,11 @@
 package com.tester.controller;
 
+import com.tester.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,6 +18,22 @@ public class Demo {
     @ApiOperation(value = "获取用户数" , httpMethod = "Get")
     public int getUserCount(){
         return template.selectOne("getUserCount");
+    }
+
+    @RequestMapping(value = "/addUser" , method = RequestMethod.POST)
+    @ApiOperation(value = "添加用户", httpMethod = "POST")
+    public int addUser(@RequestBody User user){
+        return template.insert("addUser" , user);
+    }
+
+    @RequestMapping(value = "/updataUser" , method = RequestMethod.POST)
+    public int updataUser(@RequestBody User user){
+        return template.update("updataUser" , user);
+    }
+
+    @RequestMapping(value = "/deleteUser" , method = RequestMethod.GET)
+    public int deleteUser(@RequestParam int id){
+        return template.delete("deleteUser" , id);
     }
 
 }
--- 6-mybatislogback/src/main/java/com/tester/model/User.java
+++ 6-mybatislogback/src/main/java/com/tester/model/User.java
@@ -8,4 +8,5 @@ public class User {
     private String name;
     private String sex;
     private int age;
+    private String addr;
 }
